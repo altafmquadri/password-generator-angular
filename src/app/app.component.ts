34a552d35cc65643ed0113dc53cf4a95fa354ed0
Altaf Quadri <altafmquadri@gmail.com>
2021-02-17 19:17:38 -0500

    add styling with Bulma package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
   password = '';
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = parseInt(target.value);
+    if (!isNaN(value)) this.length = value;
+  }
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
@@ -25,9 +31,40 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(this.includeLetters);
-    console.log(this.includeNumbers);
-    console.log(this.includeSymbols);
-    this.password = 'my password!!!'
+    // console.log(`
+    // About to generate a password with the following:
+    // Length is: ${this.length}
+    // Includes letters: ${this.includeLetters}
+    // Includes numbers: ${this.includeNumbers}
+    // Includes symbols: ${this.includeSymbols}
+    // `)
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    let index;
+
+    for (let i = 0; i < this.length; i++) {
+      index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
